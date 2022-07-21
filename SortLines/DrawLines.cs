7bcdfd78f90bd30d1b594d34e78d0fc64de9a678
HaimL76 <40207748+HaimL76@users.noreply.github.com>
2022-07-21 05:56:44 +0300

    first commit

---
+++ SortLines/DrawLines.cs
@@ -0,0 +1,58 @@
+﻿using SortingMethods;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Controls;
+using System.Windows.Shapes;
+
+namespace SortLines
+{
+    public class DrawLines
+    {
+        private int[] arr;
+
+        private readonly Canvas canvas;
+
+        public DrawLines(Canvas can)
+            => canvas = can;
+
+        public void AddUnsortedLines(int numLines)
+        {
+            arr = new int[numLines];
+
+            for (int i = 0; i < arr.Length; i++)
+                arr[i] = i;
+
+            arr = SelectionSort<int>.Shuffle(arr);
+
+            AddLines(canvas, arr);
+        }
+
+        public void AddSortedLines()
+        {
+            arr = new SelectionSort<int>().Sort(arr);
+
+            AddLines(canvas, arr);
+        }
+
+        public static void AddLines(Canvas canvas, int[] arr)
+        {
+            canvas.Children.Clear();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                var myLine = new Line();
+
+                myLine.Stroke = System.Windows.Media.Brushes.Black;
+
+                myLine.X1 = 2;
+                myLine.X2 = arr[i];
+                myLine.Y1 = myLine.Y2 = i * 3;
+
+                myLine.StrokeThickness = 1;
+
+                canvas.Children.Add(myLine);
+            }
+        }
+    }
+}
---
+++ SortingMethods/SelectionSort.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace SortingMethods
+{
+    public class SelectionSort<T>
+        where T: IComparable<T>
+    {
+        public static void Swap(T[] arr, int index1, int index2)
+        {
+            var temp = arr[index1];
+
+            arr[index1] = arr[index2];
+
+            arr[index2] = temp;
+        }
+
+        public static T[] Shuffle(T[] arr, int numIterations = 0)
+        {
+            var random = new Random((int)DateTime.Now.ToBinary());
+
+            if (numIterations < 1)
+                numIterations = arr.Length;
+
+            for (int i = 0; i < numIterations; i++)
+            {
+                int j = i % arr.Length;
+
+                int k = random.Next(arr.Length);
+
+                Swap(arr, j, k);
+            }
+
+            return arr;
+        }
+
+        public T[] Sort(T[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                (int MinIndex, T MinValue) tuple = (i, arr[i]);
+
+                for (int j = i + 1; j < arr.Length; j++)
+                    if (arr[j].CompareTo(tuple.MinValue) < 0)
+                        tuple = (j, arr[j]);
+
+                if (i != tuple.MinIndex)
+                {
+                    var temp = arr[i];
+
+                    arr[i] = tuple.MinValue;
+
+                    arr[tuple.MinIndex] = temp;
+                }
+            }
+
+            return arr;
+        }
+    }
+}
